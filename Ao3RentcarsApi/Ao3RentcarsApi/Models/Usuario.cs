4f4ff4c87e87ae77d1b7864f14de0fb0d147ca39
Filipe Kanitz <filipefk@gmail.com>
2020-11-27 07:58:27 -0300

    Adicionando annotations

--- Ao3RentcarsApi/Ao3RentcarsApi/Models/Usuario.cs
+++ Ao3RentcarsApi/Ao3RentcarsApi/Models/Usuario.cs
@@ -1,18 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace Ao3RentcarsApi.Models
 {
+    [Table("Usuario")]
     public class Usuario
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
-
+        [Required]
         public string Nome { get; set; }
-
+        [Required]
         public string Login { get; set; }
-
+        [Required]
         public string Senha { get; set; }
     }
 }
--- Ao3RentcarsApi/Ao3RentcarsApi/Models/Veiculo.cs
+++ Ao3RentcarsApi/Ao3RentcarsApi/Models/Veiculo.cs
@@ -1,22 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace Ao3RentcarsApi.Models
 {
+    [Table("Veiculo")]
     public class Veiculo
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
-
+        [Required]
         public string Modelo { get; set; }
-
+        [Required]
         public string Marca { get; set; }
-
+        [Required]
+        [MinLength(7)]
+        [MaxLength(7)]
         public string Placa { get; set; }
-
+        [Required]
         public int AnoModelo { get; set; }
-
+        [Required]
         public int AnoFabricacao { get; set; }
     }
 }
